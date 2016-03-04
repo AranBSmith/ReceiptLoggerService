93be38fcb811efaffce6784c20646f3e003fde53
AranBSmith <aran.smith47@mail.dcu.ie>
2016-03-04 16:37:36 +0000

    Jenkins Test

--- src/test/java/services/LoginServiceTest.java
+++ src/test/java/services/LoginServiceTest.java
@@ -24,7 +24,7 @@ public class LoginServiceTest {
 		assertNotNull(response);
 		assert(response == true || response == false);
 		response = loginService.checkCredentials(invalidEmail, invalidPassword);
-		assertTrue(response);
+		assertFalse(response);
 		response = loginService.checkCredentials(validEmail, invalidPassword);
 		assertFalse(response);
 		response = loginService.checkCredentials(invalidEmail, validPassword);
