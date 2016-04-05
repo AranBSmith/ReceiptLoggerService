8bf791999cc3d59311714849954b13f1ac6bfb48
AranBSmith <aran.smith47@mail.dcu.ie>
2016-04-05 18:42:38 +0100

    Need to add these new classes for previous commit to work.

---
+++ src/main/java/dao/DAO.java
@@ -0,0 +1,28 @@
+package dao;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+public abstract class DAO {
+	protected DataSource getMySQLDataSource() {
+        Properties props = new Properties();
+        FileInputStream fis = null;
+        MysqlDataSource mysqlDS = null;
+        try {
+            fis = new FileInputStream("db.properties");
+            props.load(fis);
+            mysqlDS = new MysqlDataSource();
+            mysqlDS.setURL(props.getProperty("MYSQL_DB_URL"));
+            mysqlDS.setUser(props.getProperty("MYSQL_DB_USERNAME"));
+            mysqlDS.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return mysqlDS;
+    }
+}
---
+++ src/main/java/dao/UserDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+public class UserDAO {
+
+	public boolean login(String email, String password) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+}
+
