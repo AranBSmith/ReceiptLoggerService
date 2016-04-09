ee54132097c41f545ea0ca1948efeae64eddf6a5
AranBSmith <aran.smith47@mail.dcu.ie>
2016-04-09 15:48:23 +0100

    Debugging

--- src/main/java/dao/UserDAO2.java
+++ src/main/java/dao/UserDAO2.java
@@ -1,23 +1,46 @@
 package dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 import model.LoginResponse;
 
-public class UserDAO2 extends DAO {
+public class UserDAO2{
 
 	private DataSource dataSource;
 	private LoginResponse loginResponse;
 
 	public UserDAO2(){
-		this.dataSource = super.getMySQLDataSource();
 		loginResponse = new LoginResponse();
+		this.dataSource = getMySQLDataSource();
 	}
+	
+	private DataSource getMySQLDataSource() {
+        Properties props = new Properties();
+        FileInputStream fis = null;
+        MysqlDataSource mysqlDS = null;
+        try {
+            fis = new FileInputStream("db.properties");
+            props.load(fis);
+            mysqlDS = new MysqlDataSource();
+            mysqlDS.setURL(props.getProperty("MYSQL_DB_URL"));
+            mysqlDS.setUser(props.getProperty("MYSQL_DB_USERNAME"));
+            mysqlDS.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            loginResponse.setResponse(loginResponse.getResponse() + " " + e.getMessage());
+        }
+        return mysqlDS;
+    }
 	
 	public LoginResponse login(String email, String password) {
 		String sql = "select password from Users where email = ?";
